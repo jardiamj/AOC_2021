b14a428b7e825acde3d94987428fbd54402e8334
jardiamj <1088732+jardiamj@users.noreply.github.com>
2021-12-22 12:56:02 -0800

    Change Compare class for priority queue to accepting a reverse flag

--- cpp/day15/main.cpp
+++ cpp/day15/main.cpp
@@ -25,18 +25,24 @@ struct Cell {
 
 class Compare {
 public:
-    bool operator()(const shared_ptr<Cell> a, const shared_ptr<Cell> b) {
-        return a->risk > b->risk;
+    Compare(bool reverse = false) : reverse(reverse) {}
+
+    bool operator()(const Cell& a, const Cell& b) {
+        if (reverse) return a.risk > b.risk;
+        return a.risk < b.risk;
     }
+
+private:
+    bool reverse;
 };
 
 void print_risk_map(const vector<vector<int>>& risk_map);
-void print_policy(const vector<vector<shared_ptr<Cell>>>& policy);
+void print_policy(const vector<vector<Cell>>& policy);
 void find_shortest_path(const vector<vector<int>>& risk_map,
-                        vector<vector<shared_ptr<Cell>>>& policy);
+                        vector<vector<Cell>>& policy);
 bool is_valid(int row, int col, int max_row, int max_col);
-void print_actions(const vector<vector<shared_ptr<Cell>>>& policy);
-void print_risk(const vector<vector<shared_ptr<Cell>>>& policy);
+void print_actions(const vector<vector<Cell>>& policy);
+void print_risk(const vector<vector<Cell>>& policy);
 void expand(vector<vector<int>>& risk_map, int multiplier);
 
 int main(int argc, char* argv[]) {
@@ -63,13 +69,13 @@ int main(int argc, char* argv[]) {
     expand(risk_map, 4);
 
     // create the policy matrix
-    vector<vector<shared_ptr<Cell>>> policy;
+    vector<vector<Cell>> policy;
     for (int row = 0; row < risk_map.size(); row++) {
-        vector<shared_ptr<Cell>> cell_row;
+        vector<Cell> cell_row;
         for (int col = 0; col < risk_map[0].size(); col++) {
-            shared_ptr<Cell> new_cell(new Cell);
-            new_cell->row = row;
-            new_cell->col = col;
+            Cell new_cell;
+            new_cell.row = row;
+            new_cell.col = col;
             cell_row.push_back(new_cell);
         }
         policy.push_back(cell_row);
@@ -83,32 +89,32 @@ int main(int argc, char* argv[]) {
 }
 
 void find_shortest_path(const vector<vector<int>>& risk_map,
-                        vector<vector<shared_ptr<Cell>>>& policy)
+                        vector<vector<Cell>>& policy)
 {
     int max_row = risk_map.size() - 1;
     int max_col = risk_map[0].size() - 1;
     int goal[2] = {max_row, max_col};
-    priority_queue<shared_ptr<Cell>, vector<shared_ptr<Cell>>, Compare> open;
-    policy[0][0]->risk = 0;
+    priority_queue<Cell, vector<Cell>, Compare> open(Compare(true));
+    policy[0][0].risk = 0;
     open.push(policy[0][0]);
     while (!open.empty()) {
         auto current = open.top(); open.pop();
-        policy[current->row][current->col]->visited = true;
-        if (current->row == goal[0] && current->col == goal[1]) {
-            cout << "Smallest total risk to goal is: " << current->risk << endl;
+        policy[current.row][current.col].visited = true;
+        if (current.row == goal[0] && current.col == goal[1]) {
+            cout << "Smallest total risk to goal is: " << current.risk << endl;
             return; // we have reached our goal
         }
 
         for (int act = 0; act < 4; act++) {
-            int next_row = current->row + delta[act][0];
-            int next_col = current->col + delta[act][1];
+            int next_row = current.row + delta[act][0];
+            int next_col = current.col + delta[act][1];
             if (is_valid(next_row, next_col, max_row, max_col) &&
-                !policy[next_row][next_col]->visited) {
+                !policy[next_row][next_col].visited) {
                 // Explore unvisited valid vertices
-                int next_risk = current->risk + risk_map[next_row][next_col];
-                if (next_risk < policy[next_row][next_col]->risk) {
-                    policy[next_row][next_col]->risk = next_risk;
-                    policy[next_row][next_col]->action = act;
+                int next_risk = current.risk + risk_map[next_row][next_col];
+                if (next_risk < policy[next_row][next_col].risk) {
+                    policy[next_row][next_col].risk = next_risk;
+                    policy[next_row][next_col].action = act;
                     // found a path with smaller risk so we add
                     // vertex to the open priority queue
                     open.push(policy[next_row][next_col]);
@@ -156,30 +162,30 @@ void print_risk_map(const vector<vector<int>>& risk_map) {
     }
 }
 
-void print_policy(const vector<vector<shared_ptr<Cell>>>& policy) {
+void print_policy(const vector<vector<Cell>>& policy) {
     for (auto row : policy) {
         for (auto cell : row) {
-            cout << "<" << cell->risk
-                 << "," << cell->visited
-                 << "," << cell->action << ">";
+            cout << "<" << cell.risk
+                 << "," << cell.visited
+                 << "," << cell.action << ">";
         }
         cout << endl;
     }
 }
 
-void print_actions(const vector<vector<shared_ptr<Cell>>>& policy) {
+void print_actions(const vector<vector<Cell>>& policy) {
     for (auto row : policy) {
         for (auto cell : row) {
-            cout << delta_name[cell->action];
+            cout << delta_name[cell.action];
         }
         cout << endl;
     }
 }
 
-void print_risk(const vector<vector<shared_ptr<Cell>>>& policy) {
+void print_risk(const vector<vector<Cell>>& policy) {
     for (auto row : policy) {
         for (auto cell : row) {
-            cout << "[" << cell->risk << "]";
+            cout << "[" << cell.risk << "]";
         }
         cout << endl;
     }
